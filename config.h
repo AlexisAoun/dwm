fdb99750987c52b60c33cdd893ab36652182772b
Alexis Aoun <alexis.aoun@hotmail.com>
2021-11-23 17:25:09 +0100

    feat: first window rules

--- config.h
+++ config.h
@@ -40,7 +40,9 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 0,       0,           -1 },
+	{ "KeePassXC",NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Pavucontrol",  NULL,       NULL,       1 << 7,       1,           -1 },
 };
 
 /* layout(s) */
